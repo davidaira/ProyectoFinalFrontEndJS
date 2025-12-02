528b7b2266201ef08dca7b8cde33cc172bbf6d0a
davidaira <david_aira304@hotmail.com.ar>
2025-12-02 15:29:29 -0300

    Se agrego carrito.html y carrito.js, para ver el carrito en una pagina alternativa

--- js/carrito.js
+++ js/carrito.js
@@ -1,74 +1,48 @@
 // Seleccionamos los contenedores
-const productsContainer = document.querySelector(".seccion-productos");
-const commentsContainer = document.querySelector(".seccion-reseñas");
+const elegidosContainer = document.querySelector(".seccion-elegidos");
+
+let carrito = sessionStorage.getItem('carrito')
 
 // Llamamos a la API y procesamos los datos
 fetch("https://dummyjson.com/products")
   .then(response => response.json())
   .then(data => {
-    // Tomamos 6 productos con el método slice.
+    // Tomamos 8 productos con el método slice.
     const products = data.products.slice(0, 8);
 
-    // Renderizamos los productos
-    let productsHTML = "";
+    // Renderizamos los productos elegidos en carrito2
+    console.log("Este es el carrito: " + carrito)
+    let elegidosHTML = "";
     for (let i = 0; i < products.length; i++) {
-      const prod = products[i];
+        const prod = products[i];
 
-      productsHTML += `
+        if(carrito.includes(prod.id)){
+
+        elegidosHTML += `
         <article class="producto">
           <h2>${prod.title}</h2>
           <img src="${prod.thumbnail}" class="img-producto" alt="${prod.title}">
           <p>${prod.description.slice(0, 50)}</p>
           <h3>$${prod.price}</h3>
-          <div><button data-id="${prod.id}" class="compra">añadir al carrito</button></div>
         </article>
-      `;
+        `;
+        }
     }
-    productsContainer.innerHTML = productsHTML;
+    elegidosContainer.innerHTML = elegidosHTML;
 
     // Activar eventos
     cargarEventosAgregar();
     actualizarContador();
 
-    // Tomamos reseñas
-    let reviews = [];
-    for (let i = 0; i < products.length; i++) {
-      const prod = products[i];
-
-      if (prod.reviews) {
-        reviews = reviews.concat(prod.reviews);
-      }
-    }
-
-    // Limitamos a 5 reseñas
-    reviews = reviews.slice(0, 5);
-
-    // Renderizamos las reseñas
-    let reviewsHTML = "";
-    for (let i = 0; i < reviews.length; i++) {
-      const review = reviews[i];
-
-      reviewsHTML += `
-        <div class="reseña">
-          <h4>${review.reviewerName}</h4>
-          <p>${review.comment}</p>
-          <h5>Fecha: ${new Date(review.date).toLocaleDateString()}</h5>
-        </div>
-      `;
-    }
-    commentsContainer.innerHTML = reviewsHTML;
-
-    
-
   })
   .catch(error => console.log("Error cargando la API:", error));
 
-// ===========================================
+  // ===========================================
 // FUNCIÓN PARA OBTENER EL CARRITO ACTUAL
 // ===========================================
 
 function obtenerCarrito() {
-  return JSON.parse(sessionStorage.getItem('carrito')) || [];
+  return JSON.parse(sessionStorage.getItem('carrito')) || []; //probamos con session para que cargue el carrito en carrito.html
 }
 
 // ================================================
@@ -77,18 +51,6 @@ function obtenerCarrito() {
 
 function guardarCarrito(carrito) {
   sessionStorage.setItem('carrito', JSON.stringify(carrito)); //se guarda en sesion para utilizar en carrito.html
-}
-
-// ==========================================
-// FUNCIÓN PARA AGREGAR PRODUCTO AL CARRITO
-// ==========================================
-
-function agregarAlCarrito(id) {
-  let carrito = obtenerCarrito();
-  carrito.push(id); // Podrías guardar el objeto completo si quisieras
-  guardarCarrito(carrito);
-  actualizarContador();
-  alert('Producto agregado al carrito');
 }
 
 // =====================================================
