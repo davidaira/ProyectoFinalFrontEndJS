7089c8cd93e9ff5d2c55d116f80e916e7ae51ba1
davidaira <david_aira304@hotmail.com.ar>
2025-12-04 12:08:33 -0300

    Se agrego la funcion para mostrar la cantidad de cada producto en el carrito, y el total de los productos en el carrito

--- js/carrito.js
+++ js/carrito.js
@@ -1,7 +1,10 @@
 // Seleccionamos los contenedores
 const elegidosContainer = document.querySelector(".seccion-elegidos");
+const elegidosTotal = document.querySelector(".elegidosTotal");
 
-let carrito = sessionStorage.getItem('carrito')
+let carrito = sessionStorage.getItem('carrito');
+
+let contador = sessionStorage.getItem('contador');
 
 // Llamamos a la API y procesamos los datos
 fetch("https://dummyjson.com/products")
@@ -10,24 +13,35 @@ fetch("https://dummyjson.com/products")
     // Tomamos 8 productos con el método slice.
     const products = data.products.slice(0, 8);
 
-    // Renderizamos los productos elegidos en carrito2
-    console.log("Este es el carrito: " + carrito)
+    // Renderizamos los productos elegidos en carrito
     let elegidosHTML = "";
-    for (let i = 0; i < products.length; i++) {
+
+    if (carrito) {
+      for (let i = 0; i < products.length; i++) {
         const prod = products[i];
 
-        if(carrito.includes(prod.id)){
+        if (carrito.includes(prod.id)) {
 
-        elegidosHTML += `
+          const count = contar(carrito, prod.id);
+
+          elegidosHTML += `
         <article class="producto">
           <h2>${prod.title}</h2>
           <img src="${prod.thumbnail}" class="img-producto" alt="${prod.title}">
           <p>${prod.description.slice(0, 50)}</p>
           <h3>$${prod.price}</h3>
+          <h3>Cantidad: ${count}</h3>
         </article>
         `;
         }
+      }
     }
+    else {
+      elegidosHTML = `
+          <h2>No hay productos en el carrito</h2>
+          `;
+    }
+
     elegidosContainer.innerHTML = elegidosHTML;
 
     // Activar eventos
@@ -37,7 +51,7 @@ fetch("https://dummyjson.com/products")
   })
   .catch(error => console.log("Error cargando la API:", error));
 
-  // ===========================================
+// ===========================================
 // FUNCIÓN PARA OBTENER EL CARRITO ACTUAL
 // ===========================================
 
@@ -87,3 +101,11 @@ document.getElementById('vaciar-carrito').addEventListener('click', () => {
   actualizarContador();
   alert('Carrito vaciado correctamente');
 });
+
+// ======================================================
+// FUNCIÓN PARA CONTAR LAS VECES QUE APARECE UN PRODUCTO EN EL CARRITO
+// ======================================================
+
+function contar(string, caracter) {
+  return string.split(caracter).length - 1;
+}
